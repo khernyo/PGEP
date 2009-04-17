63a976ce6a3039eaa013779f861b14a053e3a207
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2009-04-17 18:35:43 +0200

    ...

--- src/pgep/MatingProbabilityFunction.scala
+++ src/pgep/MatingProbabilityFunction.scala
@@ -18,3 +18,30 @@ abstract class MatingProbabilityFunction {
     totalMatingProbability
   }
 }
+
+class DirectFitnessToProbability extends MatingProbabilityFunction {
+  protected override def calculateMatingProbability(min: Double, max: Double, median: Double, fitness: Double) = min + max - fitness
+}
+
+class SogartarParabolicMappingFitnessToProbability extends MatingProbabilityFunction {
+  protected override def calculateMatingProbability(min: Double, max: Double, median: Double, fitness: Double) =
+    if (min == max) 1
+    else if (median <= fitness) 0.5 - Math.sqrt((fitness - median) / (4 * (max - median)))
+      	 else 0.5 + Math.sqrt((median - fitness) / (4 * (median - min)))
+}
+
+class SogartarEllipticMappingFitnessToProbability extends MatingProbabilityFunction {
+  protected override def calculateMatingProbability(min: Double, max: Double, median: Double, fitness: Double) =
+    if (min == max) 1
+    else if (median <= fitness) 0.5 - Math.sqrt((fitness - median) * (2 * max - median - fitness)) / (2 * (max - median))
+    	 else 0.5 + Math.sqrt((median - fitness) * (median - 2 * min + fitness)) / (2 * (median - min))
+}
+
+class SogartarSemiEllipticSemiParabolic extends MatingProbabilityFunction {
+  protected override def calculateMatingProbability(min: Double, max: Double, median: Double, fitness: Double) =
+	if (min == max) 1
+	else if (median <= fitness) (1 - Math.sqrt((fitness - median) * (2 * max - median - fitness)) / (2 * (max - median)) -
+                                 Math.sqrt((fitness - median) / (4 * (max - median)))) / 2
+         else (1 + Math.sqrt((median - fitness) * (median - 2 * min + fitness)) / (2 * (median - min)) +
+               Math.sqrt((median - fitness) / (4 * (median - min)))) / 2
+}
